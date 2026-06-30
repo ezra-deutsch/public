5db11437e592ed603c76b92e3edcc29184d61cde
Ezra Deutsch <88510296+ezra-deutsch@users.noreply.github.com>
2026-06-30 18:34:41 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
 ./main.sh
 # hello world
+
+
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("some text", TextType.LINK, "https://www.example.com")
+    print(node)
+
+main()
